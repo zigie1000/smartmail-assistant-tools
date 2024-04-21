f8f42cef8935b1acf0a64a274c83e286d63c1fa5
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-04-21 16:11:06 +0530

    Update index.js

--- tools/temp-mail/index.js
+++ tools/temp-mail/index.js
@@ -1,1 +1,248 @@
-var _0x1fbad7=_0x2249;(function(_0xce757c,_0xfc4bda){var _0x5c3ad6=_0x2249,_0xb42bcf=_0xce757c();while(!![]){try{var _0x2f922f=-parseInt(_0x5c3ad6(0x202))/(0x6*0x3be+0xdb4+-0x2427)+parseInt(_0x5c3ad6(0x1ac))/(0x2306*0x1+0x13ee+-0x1b79*0x2)+parseInt(_0x5c3ad6(0x1e4))/(0x1e03+-0x22b4*0x1+0x2b*0x1c)*(-parseInt(_0x5c3ad6(0x146))/(0xc0+0x1493+0x5*-0x443))+parseInt(_0x5c3ad6(0x1da))/(0x1d9f+-0x1*0x1d15+-0x85)*(-parseInt(_0x5c3ad6(0x1f8))/(-0x1ebb*-0x1+-0xcf6+-0x11bf))+-parseInt(_0x5c3ad6(0x1cc))/(0x2f8+-0x25*-0x49+-0x16*0x9d)*(-parseInt(_0x5c3ad6(0x138))/(-0x32*0x28+0xed2+-0x26*0x2f))+parseInt(_0x5c3ad6(0x1b8))/(-0x1623+-0x90a*-0x2+0x418)+parseInt(_0x5c3ad6(0x17d))/(-0x1*0x17f3+0x25*-0x106+0x5*0xc5f)*(parseInt(_0x5c3ad6(0x161))/(-0x629*-0x1+-0x4a*-0x40+-0x189e));if(_0x2f922f===_0xfc4bda)break;else _0xb42bcf['push'](_0xb42bcf['shift']());}catch(_0x559c72){_0xb42bcf['push'](_0xb42bcf['shift']());}}}(_0x56e9,0x254b2+0x15613+-0x22194));import{fetchMessages,getMessage}from'./req_module.js';var copyBtn=document[_0x1fbad7(0x192)+_0x1fbad7(0x1b0)](_0x1fbad7(0x1d3)),genAnotherMail=document[_0x1fbad7(0x192)+_0x1fbad7(0x1b0)](_0x1fbad7(0x1cd)),eminp=document[_0x1fbad7(0x192)+_0x1fbad7(0x1b0)](_0x1fbad7(0x197)),emailList=[_0x1fbad7(0x18a)+'ne',_0x1fbad7(0x1c4)+'wo',_0x1fbad7(0x1c4)+_0x1fbad7(0x1b9)],lastChosen,nowUsing;let charList=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],domainList=[_0x1fbad7(0x174),_0x1fbad7(0x14b),_0x1fbad7(0x176)],lastDomUsed='',lastRefreshTime,refreshTimer=document[_0x1fbad7(0x192)+_0x1fbad7(0x1b0)](_0x1fbad7(0x1b4)+_0x1fbad7(0x1f2)),rerate=0x1*0xe3a+-0x1ec8+-0x1*-0x1098,refreshInterval;function ramdomEmail(_0x1c330f){var _0x314fc7=_0x1fbad7,_0x4b9ebf={'DNPYZ':function(_0x4fbfeb,_0x5dfb9a){return _0x4fbfeb*_0x5dfb9a;},'VdCmR':function(_0xf8cc2,_0x33fd83){return _0xf8cc2>_0x33fd83;},'hKmPr':function(_0x3c8b1b,_0x3b0dd6){return _0x3c8b1b===_0x3b0dd6;}};let _0x5871ab='',_0x2f4ee1='';do{_0x5871ab+=charList[Math[_0x314fc7(0x1b7)](_0x4b9ebf[_
+import { fetchMessages, getMessage } from "./req_module.js";
+
+
+var copyBtn = document.querySelector("#copyBtn"),
+  genAnotherMail = document.querySelector("#genbtn"),
+  eminp = document.querySelector("#prmpt"),
+  emailList = ["somthing.one", 'somthing.two', 'somthing.three'],
+  lastChosen,
+  nowUsing;
+let charList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  domainList = ["txcct.com", "laafd.com", "vjuum.com"],
+  lastDomUsed = '',
+  lastRefreshTime,
+  refreshTimer = document.querySelector("#refreshTimer");
+  let rerate = 10;
+  let refreshInterval;
+
+function ramdomEmail(len) {
+  let toReturn = "",
+    finalDomToReturn = '';
+  do {
+    toReturn += charList[Math.floor(Math.random() * charList.length)];
+    len--;
+  } while (len > 0);
+  toReturn += "@";
+
+  do {
+    finalDomToReturn = domainList[Math.floor(Math.random() * domainList.length)];
+    console.log(finalDomToReturn);
+  } while (lastDomUsed === finalDomToReturn)
+  toReturn += finalDomToReturn;
+  lastDomUsed = finalDomToReturn;
+  return toReturn;
+}
+
+var genAFORNOWTEMP = ramdomEmail(5);
+eminp.value = genAFORNOWTEMP;
+var nowUsing = genAFORNOWTEMP;
+eminp.addEventListener("input", (e) => {
+  eminp.value = nowUsing;
+});
+/*
+var mefetch = await fetchMessages(nowUsing);
+clearInbox();
+mefetch.forEach((data) => {
+sendToInbox({
+  email: nowUsing,
+  id: data.id,
+  title: data.subject,
+  time: data.date,
+  sender: data.from
+});
+});*/
+checkInbox();
+
+async function checkInbox() {
+  let mefetchho = await fetchMessages(nowUsing);
+  clearInbox();
+  mefetchho.forEach(retdata => {
+    sendToInbox({
+      email: nowUsing,
+      id: retdata.id,
+      title: retdata.subject,
+      time: retdata.date,
+      sender: retdata.from
+    });
+  });
+  
+  if(mefetchho.length < 1) {
+  document.querySelector(".inbox").innerHTML = `<span style="font-size: 30px; color: #02f006; cursor: wait;">
+        nothing in the inbox, inbox automatically refresh in every 10 seconds
+      </span>`;
+  }
+}
+
+function clearInbox() {
+  document.querySelector(".inbox").innerHTML = "";
+}
+function sendToInbox(mail, email) {
+  var ele37 = document.createElement("div");
+  ele37.classList.add("inboxMail");
+  ele37.onclick = function() {
+    getMessage(`${mail?.id ?? null}`, `${mail?.email}`);
+  }
+  var epe74 = document.createElement("span");
+  epe74.classList.add("mailTitle");
+  epe74.textContent = mail?.title ?? "Unknown Title";
+  epe74.onclick = function() {
+    getMessage(`${mail?.id ?? null}`, `${mail?.email}`);
+  }
+  ele37.appendChild(epe74);
+  ele37.appendChild(document.createElement("br"));
+  ele37.appendChild(document.createElement("br"));
+  var ehi94 = document.createElement("span");
+  ehi94.classList.add("sendBy");
+  ehi94.textContent = mail?.sender ?? "Unknown Sender";
+  ele37.appendChild(ehi94);
+  ele37.appendChild(document.createElement("br"));
+  var tme32 = document.createElement("span");
+  tme32.classList.add("mailTime");
+  tme32.textContent = mail?.time ?? "00/00/0000 | 24:00";
+  ele37.appendChild(tme32);
+  document.querySelector(".inbox").appendChild(ele37);
+}
+
+const cookieConsent = document.getElementById('cookie-consent');
+try {
+const acceptButton = document.getElementById('accept-cookies');
+
+acceptButton.addEventListener('click', () => {
+  // Simulate storing consent (replace with actual storage logic)
+  localStorage.setItem('cookieConsentAccepted', true);
+  cookieConsent.style.display = 'none';
+}).catch();
+// } catch {}
+
+if (localStorage.getItem('cookieConsentAccepted')) {
+  cookieConsent.style.display = 'none';
+}
+} catch (e) {console.log(e);}
+
+copyBtn.addEventListener("click", () => {
+  copyBtn.textContent = 'email address copied !..';
+  copyBtn.style.cursor = "grabbing";
+  copyBtn.disabled = true;
+  try {
+    eminp.select();
+    eminp.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(eminp.value);
+  } catch {}
+  setTimeout(() => {
+    copyBtn.textContent = 'COPY EMAIL ADDRESS';
+    copyBtn.style.cursor = "grab";
+    copyBtn.disabled = false;
+  }, 1500);
+});
+
+
+let rateNewRef;
+genAnotherMail.addEventListener("click", () => {
+  let newMailAdd = ramdomEmail(5);
+  eminp.value = newMailAdd;
+  nowUsing = newMailAdd;
+  if(rateNewRef) {
+    clearTimeout(rateNewRef);
+    console.log("rate limit hut");
+  }
+  rateNewRef = setTimeout(() => {
+  clearInbox();
+  checkInbox();
+  clearTimeout(rateNewRef);
+  clearInterval(refreshInterval);
+  rerate = 11;
+  refreshInterval = setInterval(() => {
+  rerate--;
+  if (rerate < 0) {
+    rerate = 10;
+    checkInbox();
+  }
+  refreshTimer.textContent = `Auto Refresh In ${rerate} sec.`;
+}, 1001);
+
+  console.log("success");
+  }, 1800);
+});
+
+try {
+document.querySelector('.refreshBtn').addEventListener('click', () => {
+  if (lastRefreshTime+3000 > Date.now()) {
+    return;
+  }
+  lastRefreshTime = Date.now();
+  console.log(lastRefreshTime);
+  
+});
+} catch {};
+
+// auto refresh evbery 10 sec
+refreshInterval = setInterval(() => {
+  rerate--;
+  if (rerate < 0) {
+    rerate = 10;
+    checkInbox();
+  }
+  refreshTimer.textContent = `Auto Refresh In ${rerate} sec.`;
+}, 1001);
+
+document.querySelector('.inboxMsgClosebutton').addEventListener("click", () => {
+  document.querySelector(`.perEmail`).style.display = 'none';
+});
+
+function setCustom(email) {
+  nowUsing = email ?? null;
+  eminp.value = nowUsing;
+}
+
+
+var nameInp = document.querySelector("#name");
+document.querySelector("#name").addEventListener("input", () => {
+  if(!(/^[a-zA-Z0-9]+$/.test(nameInp.value))){
+    var oldhdo = nameInp.value.replace(/[^a-zA-Z0-9]/g, "");
+    nameInp.value = oldhdo;
+  }
+});
+
+document.querySelector("#setCDomain").addEventListener("click", () => {
+  var name = document.querySelector("#name").value;
+  var domain = document.querySelector("#domain").value;
+  if (!name || name.trim() === "") {
+    document.querySelector("#name").focus();
+    return;
+  }
+  document.querySelector(".customEmailDiv").style.display = "none";
+  var email = name + "@" + domain;
+setCustom(email);
+
+if(rateNewRef) {
+    clearTimeout(rateNewRef);
+    console.log("rate limit hut");
+  }
+  rateNewRef = setTimeout(() => {
+  clearInbox();
+  checkInbox();
+  clearTimeout(rateNewRef);
+  clearInterval(refreshInterval);
+  rerate = 11;
+  refreshInterval = setInterval(() => {
+  rerate--;
+  if (rerate < 0) {
+    rerate = 10;
+    checkInbox();
+  }
+  refreshTimer.textContent = `Auto Refresh In ${rerate} sec.`;
+}, 1001);
+
+  console.log("success");
+  }, 1800);
+});
+
+document.querySelector("#custombtn").addEventListener("click", () => {
+  var ced =   document.querySelector(".customEmailDiv");
+  if (ced.style.display == 'block') {
+    return;
+  }
+  ced.style.display = "block";
+});
+
+
