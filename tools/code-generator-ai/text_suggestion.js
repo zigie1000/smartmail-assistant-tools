7545126230b97c11606f9d4f50f527ed25119ff3
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-05-07 21:32:37 +0530

    Update text_suggestion.js

--- tools/code-generator-ai/text_suggestion.js
+++ tools/code-generator-ai/text_suggestion.js
@@ -4,6 +4,7 @@ var suggestions = [
   "Create a web server in flask with some usefull routes",
   "Create a Discord bot in JavaScript with commands and events organized into separate files and folders, and provide a clear folder-file structure."
   ];
+var randomSuggestion;
 var textInp = document.querySelector("#prmpt");
 textInp.addEventListener("focus", () => {
 var randomSuggestion = suggestions[Math.floor(Math.random() * suggestions.length)];
@@ -13,4 +14,10 @@ textInp.placeholder = randomSuggestion + " \n\n𝙋𝙧𝙚𝙨𝙨 🆃🅰🅱
 // 𝙒𝙧𝙞𝙩𝙚 𝙒𝙝𝙖𝙩 𝙔𝙤𝙪 𝙒𝙖𝙣𝙩 𝙏𝙤 𝙂𝙚𝙣𝙚𝙧𝙖𝙩𝙚
 textInp.addEventListener("blur", () => {
 textInp.placeholder = "𝙒𝙧𝙞𝙩𝙚 𝙒𝙝𝙖𝙩 𝙔𝙤𝙪 𝙒𝙖𝙣𝙩 𝙏𝙤 𝙂𝙚𝙣𝙚𝙧𝙖𝙩𝙚 \n\n𝘾𝙡𝙞𝙘𝙠 𝙏𝙤 𝙂𝙚𝙩 𝙎𝙤𝙢𝙚 𝙎𝙪𝙜𝙜𝙚𝙨𝙩𝙞𝙤𝙣";
+});
+
+textInp.addEventListener("keydown", (event) => {
+if(event.keyCode === 13 && textInp.value === ""){
+textInp.value = randomSuggestion ?? "";
+}
 });
