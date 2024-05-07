032969543a8bcd614ea9324332d23a8d162a3f3f
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-05-07 21:23:58 +0530

    Update text_suggestion.js

--- tools/code-generator-ai/text_suggestion.js
+++ tools/code-generator-ai/text_suggestion.js
@@ -7,10 +7,10 @@ var suggestions = [
 var textInp = document.querySelector("#prmpt");
 textInp.addEventListener("focus", () => {
 var randomSuggestion = suggestions[Math.floor(Math.random() * suggestions.length)];
-textInp.placeholder = randomSuggestion + " \n\n𝙋𝙧𝙚𝙨𝙨 🆃🅰🅱 𝙠𝙚𝙮 𝙩𝙤 𝙪𝙨𝙚 𝙩𝙝𝙞𝙨 𝙨𝙪𝙜𝙜𝙚𝙨𝙩𝙞𝙤𝙣  ";
+textInp.placeholder = randomSuggestion + " \n\n𝙋𝙧𝙚𝙨𝙨 🆃🅰🅱 𝙠𝙚𝙮 𝙩𝙤 𝙪𝙨𝙚 𝙩𝙝𝙞𝙨 𝙨𝙪𝙜𝙜𝙚𝙨𝙩𝙞𝙤𝙣 \𝙣𝙋𝙧𝙚𝙨𝙨 🅴🅽🆃🅴🆁 𝙠𝙚𝙮 𝙩𝙤 𝙨𝙝𝙪𝙛𝙛𝙡𝙚";
 });
 
 // 𝙒𝙧𝙞𝙩𝙚 𝙒𝙝𝙖𝙩 𝙔𝙤𝙪 𝙒𝙖𝙣𝙩 𝙏𝙤 𝙂𝙚𝙣𝙚𝙧𝙖𝙩𝙚
 textInp.addEventListener("blur", () => {
-textInp.placeholder = "𝙒𝙧𝙞𝙩𝙚 𝙒𝙝𝙖𝙩 𝙔𝙤𝙪 𝙒𝙖𝙣𝙩 𝙏𝙤 𝙂𝙚𝙣𝙚𝙧𝙖𝙩𝙚";
+textInp.placeholder = "𝙒𝙧𝙞𝙩𝙚 𝙒𝙝𝙖𝙩 𝙔𝙤𝙪 𝙒𝙖𝙣𝙩 𝙏𝙤 𝙂𝙚𝙣𝙚𝙧𝙖𝙩𝙚 \n\n𝘾𝙡𝙞𝙘𝙠 𝙏𝙤 𝙂𝙚𝙩 𝙎𝙤𝙢𝙚 𝙎𝙪𝙜𝙜𝙚𝙨𝙩𝙞𝙤𝙣";
 });
